1a53c7aa3a3c3780901fd752c4369c6b36ffac59
SonDaeHong <dnddl505@gmail.com>
2021-12-09 18:42:27 +0900

    api 호출 test print

--- AirQualityMVVM/Presentation/ViewController.swift
+++ AirQualityMVVM/Presentation/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let sampleRepository: SampleRepository = DefaultSampleRepository()
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("viewDidLoad")
         // Do any additional setup after loading the view.
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        print("viewWillAppear")
+        sampleRepository.fetchTest(latitude: "37.5099983215332", longitude: "127.01000213623047") {
+            print($0 ?? "")
+        }
+    }
 
 }
 
