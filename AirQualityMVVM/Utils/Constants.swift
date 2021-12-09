511e5056ededb5d2935116b18cff0d874f9d70e1
SonDaeHong <dnddl505@gmail.com>
2021-12-09 18:40:33 +0900

    api constants 추가

---
+++ AirQualityMVVM/Utils/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  AirQualityMVVM
+//
+//  Created by 코액터스 on 2021/12/09.
+//
+
+import Foundation
+
+enum API {
+    enum REVERSE_GEOCODE {
+        static let BASE_URL = "https://api.bigdatacloud.net"
+        static let PATH = "/data/reverse-geocode-client"
+    }
+}
