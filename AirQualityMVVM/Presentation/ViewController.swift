8c6d8711d99eb95817f4b94b64dca867ec753c3d
SonDaeHong <dnddl505@gmail.com>
2021-12-13 18:58:27 +0900

    vc - vm 바인딩

--- AirQualityMVVM/Presentation/ViewController.swift
+++ AirQualityMVVM/Presentation/ViewController.swift
@@ -6,25 +6,50 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
 
-    private let sampleRepository: SampleRepository = DefaultSampleRepository()
+
+    private var sampleViewModel: ViewModel!
+    private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        sampleViewModel = makeSampleViewModel()
+        bind(to: sampleViewModel)
         print("viewDidLoad")
         // Do any additional setup after loading the view.
     }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         print("viewWillAppear")
-        sampleRepository.fetchTest(latitude: "37.5099983215332", longitude: "127.01000213623047") {
-            if let administratives = $0 {
-                administratives.forEach { administrative in
-                    print(administrative)
-                }
-            }
-        }
+        sampleViewModel.sampleInput(latitude: "37.5099983215332", longitude: "127.01000213623047")
+        sampleViewModel.sampleInput(latitude: "0", longitude: "0")
+    }
+    
+    // MARK: - private methods
+    
+    private func bind(to viewModel: ViewModel){
+        viewModel.result.subscribe(onNext: {
+            print($0)
+        }).disposed(by: disposeBag)
+    }
+    
+    // MARK: - factory methods, 이동 예정
+    
+    private func makeSampleViewModel() -> ViewModel {
+        return DefaultViewModel(sampleUseCase: makeSampleUseCase())
+    }
+    
+    private func makeSampleUseCase() -> SampleUseCase {
+        return DefaultSampleUseCase(sampleRepository: makeSampleRepository())
+    }
+    
+    private func makeSampleRepository() -> SampleRepository {
+        return DefaultSampleRepository()
     }
 
 }
