62cff1ff8f2e1ea26d373100f0edd5ad4e54b46b
SonDaeHong <dnddl505@gmail.com>
2021-12-15 18:21:11 +0900

    Air Quality Index Constants 추가

--- AirQualityMVVM/Utils/Constants.swift
+++ AirQualityMVVM/Utils/Constants.swift
@@ -12,4 +12,10 @@ enum API {
         static let BASE_URL = "https://api.bigdatacloud.net"
         static let PATH = "/data/reverse-geocode-client"
     }
+    
+    enum AIR_QUALITY_INDEX {
+        static let BASE_URL = "https://api.waqi.info"
+        static let PATH = "/feed/geo"
+        static let TOKEN = "809ef3190596db15041a647ed0db24c5deb874e3"
+    }
 }
