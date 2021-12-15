04fad9a2c0b333888070273190b4cb8cba4e8453
SonDaeHong <dnddl505@gmail.com>
2021-12-15 18:20:48 +0900

    Air Quality Index DTO 추가

---
+++ AirQualityMVVM/Data/Network/DataMapping/AqiRequestDTO.swift
@@ -0,0 +1,15 @@
+//
+//  AqiRequestDTO.swift
+//  AirQualityMVVM
+//
+//  Created by 코액터스 on 2021/12/15.
+//
+
+import Foundation
+
+struct AqiRequestDTO: Encodable {
+    
+    init() {
+        
+    }
+}
---
+++ AirQualityMVVM/Data/Network/DataMapping/AqiResponseDTO.swift
@@ -0,0 +1,18 @@
+//
+//  AqiResponseDTO.swift
+//  AirQualityMVVM
+//
+//  Created by 코액터스 on 2021/12/15.
+//
+
+import Foundation
+
+struct AqiResponseDTO: Decodable {
+    
+}
+
+extension AqiResponseDTO {
+    func toDomain() -> String {
+        return ""
+    }
+}
