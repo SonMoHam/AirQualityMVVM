f7be70d082698d9c48db8f4d0fe3c3640ef4dd9a
SonDaeHong <dnddl505@gmail.com>
2021-12-15 18:32:36 +0900

    headers 추출

--- AirQualityMVVM/Data/Network/APIEndpoints.swift
+++ AirQualityMVVM/Data/Network/APIEndpoints.swift
@@ -8,18 +8,19 @@
 import Foundation
 
 struct APIEndpoints {
+    static private let headers = ["Content-Type": "application/json"]
     static func getTest(with sampleRequestDTO: SampleRequestDTO) -> Endpoint<SampleResponseDTO> {
         return Endpoint(baseURL: API.REVERSE_GEOCODE.BASE_URL,
                         path: API.REVERSE_GEOCODE.PATH,
                         method: .get,
                         queryParameters: sampleRequestDTO,
-                        headers: ["Content-Type": "application/json"] )
+                        headers: headers)
     }
     
     static func getAQI(with aqiRequestDTO: AqiRequestDTO) -> Endpoint<AqiResponseDTO> {
         return Endpoint(baseURL: API.AIR_QUALITY_INDEX.BASE_URL,
-                        path: API.AIR_QUALITY_INDEX.PATH + ":lat;long/?token=\(API.AIR_QUALITY_INDEX.TOKEN)",
+                        path: API.AIR_QUALITY_INDEX.PATH + ":\("latitude");\("longitude")/?token=\(API.AIR_QUALITY_INDEX.TOKEN)",
                         method: .get,
-                        headers: ["Content-Type": "application/json"])
+                        headers: headers)
     }
 }
